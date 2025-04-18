e8cf39811b5c6a999a86a7b3530ad78e32caf0a1
pekkavii <pekkavii@gmail.com>
2025-04-18 10:37:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -302,7 +302,7 @@ function fetchWeather() {
 
 //https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=wind_speed_10m,wind_direction_10m,cloud_cover,is_day,rain&wind_speed_unit=ms
     
-        const url = `https://api.open-meteo.com/v1/forecast?latitude=${selectedLat}&longitude=${selectedLon}&current=is_day,cloud_cover,wind_speed_10m,wind_direction_10m,rain&timezone=auto&wind_speed_unit=ms`;
+        const url = `https://api.open-meteo.com/v1/forecast?latitude=${selectedLat}&longitude=${selectedLon}&current=wind_speed_10m,wind_direction_10m,cloud_cover,is_day,rain&wind_speed_unit=ms`;
         fetch(url)
             .then(res => res.json())
             .then(data => {
